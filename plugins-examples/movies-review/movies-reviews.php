9a5cd7328c73578708109dd5e634be5279148a11
Cláudio Hilário <claudio.hilario@ontech.pt>
2020-01-24 17:18:48 +0000

    [ADD] Config

--- plugins-examples/movies-review/movies-reviews.php
+++ plugins-examples/movies-review/movies-reviews.php
@@ -80,55 +80,37 @@ class Movies_reviews {
             ),
         );
 
-        /** Config */
-        $config = array(
-            'domain' => $this->text_domain,
-            'default_path' => '',
-            'parent_slug' => 'plugins.php',
-            'capability' => 'update_plugins',
-            'menu' => 'install-required-plugins',
-            'has_notices' => true,
-            'is_automatic' => false,
-            'message' => '',
-            'strings' => array(
-                'page_title' => __('Instalar os plugins requiridos', $this->text_domain ),
-                'menu_title' => __('Instalar plugins', $this->text_domain),
-                'installing' => __('A instalar o plugin: %s', $this->text_domain),
-                'oops' => __('Alguma coisa falhou com a API do plugin.', $this->text_domain),
-                'notice_can_install_required' => _n_noop( 
-                    'O plugin movies reviews para funcionar corretamente, depende da instalação do plugin: %1$s.', 
-                    'O plugin movies reviews para funcionar corretamente, depende da instalação dos plugins: %1$s.'),
-                'notice_can_install_recommended' => _n_noop( 
-                    'O plugin movies reviews para funcionar corretamente, depende da instalação do plugin: %1$s.', 
-                    'O plugin movies reviews para funcionar corretamente, depende da instalação dos plugins: %1$s.'),
-                'notice_cannot_install' => _n_noop( 
-                    'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed', 
-                    'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed'),
-                'notice_can_activate_required' => _n_noop( 
-                    'The following required plugin is currently inactive: %1$s.', 
-                    'The following required plugins are currently inactive: %1$s.'),
-                'notice_can_activate_recommended' => _n_noop( 
-                    'The following recommended plugin is currently inactive: %1$s.', 
-                    'The following recommended plugins are currently inactive: %1$s.'),
-                'notice_cannot_activate' => _n_noop( 
-                    'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on gettiong the plugin activated.', 
-                    'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on gettiong the plugins activated.'),
-                'notice_ask_to_update' => _n_noop( 
-                    'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 
-                    'The following plugins needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.'),
-                'notice_cannot_update' => _n_noop( 
-                    'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on gettiong the plugin activated.', 
-                    'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on gettiong the plugin activated.'),
-                'install_link' => _n_noop('Iniciar instalação do plugin', 'Iniciar instalação dos plugins'),
-                'activate_link' => _n_noop('Ativar o plugin instalado', 'Ativar os plugins instalados'),
-                'return' => __('Retornar ao plugin instalado', $this->text_domain),
-                'plugin_activated' => __('Plugin ativado com sucesso', $this->text_domain),
-                'complete' => __('All plugins installed and activated successfully. %s', $this->text_domain),
-                'nag_type' => 'updated',
-                ),
-        );
-        /** End Config */
-        tgmpa($plugins, $config);
+        $config  = array(
+            'domain'           => $this->text_domain,
+            'default_path'     => '',
+            'parent_slug'      => 'plugins.php',
+            'capability'       => 'update_plugins',
+            'menu'             => 'install-required-plugins',
+            'has_notices'      => true,
+            'is_automatic'     => false,
+            'message'          => '',
+            'strings'          => array(
+              'page_title'                      => __( 'Install Required Plugins', $this->text_domain ),
+              'menu_title'                      => __( 'Install Plugins', $this->text_domain ),
+              'installing'                      => __( 'Installing Plugin: %s', $this->text_domain ),
+              'oops'                            => __( 'Something went wrong with the plugin API.', $this->text_domain ),
+              'notice_can_install_required'     => _n_noop( 'The Movie Reviews plugin depends on the following plugin: %1$s.', 'The Movie Reviews plugin depends on the following plugins: %1$s.' ),
+              'notice_can_install_recommended'  => _n_noop( 'The Movie Reviews plugin recommends the following plugin: %1$s.', 'The Movie Reviews plugin recommends the following plugins: %1$s.' ),
+              'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ),
+              'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ),
+              'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ),
+              'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ),
+              'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ),
+              'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ),
+              'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+              'activate_link'                   => _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
+              'return'                          => __( 'Return to Required Plugins Installer', $this->text_domain ),
+              'plugin_activated'                => __( 'Plugin activated successfully.', $this->text_domain ),
+              'complete'                        => __( 'All plugins installed and activated successfully. %s', $this->text_domain ),
+              'nag_type'                        => 'updated',
+            )
+          );
+          tgmpa( $plugins, $config );
     }
 
     public static function activate() {
